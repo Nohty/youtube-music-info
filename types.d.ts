241cb212c0c66383f5f834f827c2976996736264
Daan Hautekiet <daanhautekiet@gmail.com>
2022-02-01 22:52:17 +0100

    Created type MusicInfo

--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,14 @@
 type Settings = {
-	enabled: boolean;
-	url: string;
+  enabled: boolean;
+  url: string;
+};
+
+type MusicInfo = {
+  title: string;
+  artist: string;
+  album: string;
+  year: string;
+  image: string | null;
+  url: string;
+  playing: boolean;
 };
