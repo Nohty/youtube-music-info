10cd1efabe0e512fd20d25d074fea10fa6a13a52
Daan Hautekiet <daanhautekiet@gmail.com>
2022-02-01 22:24:56 +0100

    Saved background.ts

--- src/background.ts
+++ src/background.ts
@@ -1,12 +1,12 @@
 namespace background {
-	const SETTINGS: Settings = {
-		enabled: true,
-		url: "http://localhost:3002/",
-	};
+  const SETTINGS: Settings = {
+    enabled: true,
+    url: "http://localhost:3002/",
+  };
 
-	async function saveSettings(settings: Settings) {
-		await chrome.storage.sync.set({ settings });
-	}
+  async function saveSettings(settings: Settings) {
+    await chrome.storage.sync.set({ settings });
+  }
 
-	chrome.runtime.onInstalled.addListener(() => saveSettings(SETTINGS));
+  chrome.runtime.onInstalled.addListener(() => saveSettings(SETTINGS));
 }
