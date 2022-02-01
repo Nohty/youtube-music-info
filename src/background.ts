ebf6fd9aced1713464175433cfa6b4b06312b5b4
Daan Hautekiet <daanhautekiet@gmail.com>
2022-02-01 23:32:37 +0100

    Created handleMessage function

--- src/background.ts
+++ src/background.ts
@@ -4,11 +4,22 @@ namespace background {
     url: "http://localhost:3002/",
   };
 
+  let MUSIC_INFO: MusicInfo;
+
   async function saveSettings(settings: Settings) {
     await chrome.storage.sync.set({ settings });
   }
 
+  function handleMessage(request: any, sender: any, sendResponse: any) {
+    if (request.type === "music-info") {
+      if (MUSIC_INFO !== request.musicInfo) {
+        MUSIC_INFO = request.musicInfo;
+      }
+    }
+  }
+
   chrome.runtime.onInstalled.addListener(() => saveSettings(SETTINGS));
+  chrome.runtime.onMessage.addListener(handleMessage);
 
   // TODO: Create socket.io client
 }
