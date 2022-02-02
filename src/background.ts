c1c59607de0ed152a6daf6634482a197660be3d1
Daan Hautekiet <daanhautekiet@gmail.com>
2022-02-02 14:03:48 +0100

    Created functions for creating server

--- src/background.ts
+++ src/background.ts
@@ -4,10 +4,42 @@ namespace background {
     url: "http://localhost:3002/",
   };
 
+  const MUSIC_URL = "https://music.youtube.com/";
+
+  let interval: number | null = null;
   let MUSIC_INFO: MusicInfo;
+  let SOCKET: any = null;
 
   async function saveSettings(settings: Settings) {
-    await chrome.storage.sync.set({ settings });
+    await chrome.storage.local.set({ settings });
+  }
+
+  async function handleTabUpdate(tabId: any, changeInfo: any, tab: chrome.tabs.Tab) {
+    if (tab.status === "complete") {
+      if (tab.url?.startsWith(MUSIC_URL) && !SOCKET) {
+        createSocketServer();
+        interval = setInterval(checkTabOpen, 1000);
+      }
+    }
+  }
+
+  async function checkTabOpen() {
+    console.log("checkTabOpen");
+    const tabs = await chrome.tabs.query({ url: MUSIC_URL });
+    if (tabs.length === 0) {
+      closeSocketServer();
+      if (interval) clearInterval(interval);
+    }
+  }
+
+  async function createSocketServer() {
+    console.log("Creating socket server");
+    SOCKET = true;
+  }
+
+  async function closeSocketServer() {
+    console.log("Closing socket server");
+    SOCKET = null;
   }
 
   function handleMessage(request: any, sender: any, sendResponse: any) {
@@ -20,6 +52,5 @@ namespace background {
 
   chrome.runtime.onInstalled.addListener(() => saveSettings(SETTINGS));
   chrome.runtime.onMessage.addListener(handleMessage);
-
-  // TODO: Create socket.io client
+  chrome.tabs.onUpdated.addListener(handleTabUpdate);
 }
