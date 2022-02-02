aee75478905ed7b17e5817502781080a882acf0c
Daan Hautekiet <daanhautekiet@gmail.com>
2022-02-02 16:21:55 +0100

    Created functions to start socket server

--- src/content.ts
+++ src/content.ts
@@ -11,6 +11,12 @@ namespace content {
   const image = document.querySelector<HTMLImageElement>(HTMLSelector.IMAGE);
   const playing = document.querySelector<HTMLHeadingElement>(HTMLSelector.PLAYING);
 
+  const MUSIC_URL = "https://music.youtube.com/";
+
+  let info: MusicInfo = createEmptyInfo();
+  let socket: any;
+  let interval: number;
+
   function getInformation(): MusicInfo {
     const songInfoArray = songInfo?.innerText.split("\n");
     return {
@@ -18,15 +24,57 @@ namespace content {
       artist: songInfoArray?.[0] || null,
       album: songInfoArray?.[2] || null,
       year: songInfoArray?.[4] || null,
-      image: image?.src || null,
-      url: window.location.href,
+      image: image?.src.startsWith(MUSIC_URL) ? null : image?.src!,
+      url: window.location.href === MUSIC_URL ? null : window.location.href,
       playing: playing?.title === "Pause" || false,
     };
   }
 
-  function sendInformation(musicInfo: MusicInfo) {
-    chrome.runtime.sendMessage({ type: "music-info", musicInfo });
+  function equals(obj1: Object, obj2: Object) {
+    return JSON.stringify(obj1) === JSON.stringify(obj2);
   }
 
-  setInterval(() => sendInformation(getInformation()), 1000);
+  function createEmptyInfo() {
+    return {
+      title: null,
+      artist: null,
+      album: null,
+      year: null,
+      image: null,
+      url: null,
+      playing: false,
+    };
+  }
+
+  function checkInformation() {
+    const newInfo = getInformation();
+    if (!equals(info, newInfo)) {
+      info = newInfo;
+      socket.emit("yt-music", info);
+    }
+  }
+
+  async function getSettings(): Promise<Settings> {
+    return (await chrome.storage.local.get("settings")).settings;
+  }
+
+  async function enableSocketClient() {
+    socket = io((await getSettings()).url);
+    socket.on("yt-music:update", () => socket.emit("yt-music", info));
+    interval = setInterval(checkInformation, 1000);
+  }
+
+  function disableSocketClient() {
+    socket.close();
+    clearInterval(interval);
+  }
+
+  function handleMessage(message: any) {
+    if (message.type === "init") {
+      if (message.status) enableSocketClient();
+      else disableSocketClient();
+    }
+  }
+
+  chrome.runtime.onMessage.addListener(handleMessage);
 }
